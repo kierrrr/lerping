a4c20b3fc74bf479711221d1e7e3db0265e7f102
Kier Lomugdang <klomugdang@atlassian.com>
2025-05-03 23:26:50 +1000

    Added prompts about conversations

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -7,14 +7,20 @@ try {
   const vocabPathFile = path.join(process.cwd(), "/data/vocab.csv");
   const patternsPathFile = path.join(process.cwd(), "/data/patterns.csv");
   const promptPathFile = path.join(process.cwd(), "/data/prompt.txt");
+  const promptConversationFile = path.join(
+    process.cwd(),
+    "/data/promptConversationEnglishReply.txt",
+  );
 
-  const [vocabData, patternsData, promptData] = await Promise.all([
-    fs.readFile(vocabPathFile, { encoding: "utf8" }),
-    fs.readFile(patternsPathFile, {
-      encoding: "utf8",
-    }),
-    fs.readFile(promptPathFile, { encoding: "utf8" }),
-  ]);
+  const [vocabData, patternsData, promptData, promptConversationData] =
+    await Promise.all([
+      fs.readFile(vocabPathFile, { encoding: "utf8" }),
+      fs.readFile(patternsPathFile, {
+        encoding: "utf8",
+      }),
+      fs.readFile(promptPathFile, { encoding: "utf8" }),
+      fs.readFile(promptConversationFile, { encoding: "utf8" }),
+    ]);
 
   const numberOfVocab = vocabData.split("\n").length - 1;
   const numberOfPatterns = patternsData.split("\n").length - 1;
@@ -31,9 +37,26 @@ try {
     )
     .replace("{{epoch}}", Date.now());
 
+  const finalPromptConversation = promptConversationData
+    .replaceAll("{{vocab_num}}", numberOfVocab)
+    .replaceAll("{{vocab}}", vocabData)
+    .replaceAll("{{patterns}}", patternsData)
+    .replaceAll("{{flashcards_num}}", NUM_OF_FLASHCARDS_PER_PATTERN)
+    .replaceAll("{{patterns_num}}", numberOfPatterns)
+    .replaceAll(
+      "{{flashcards_total}}",
+      numberOfPatterns * NUM_OF_FLASHCARDS_PER_PATTERN,
+    )
+    .replace("{{epoch}}", Date.now());
+
   await fs.writeFile(
     path.join(process.cwd(), "/output/prompt.txt"),
     finalPrompt,
+  );
+
+  await fs.writeFile(
+    path.join(process.cwd(), "/output/promptConversation.txt"),
+    finalPromptConversation,
   );
   console.log("DONE");
 } catch (err) {
